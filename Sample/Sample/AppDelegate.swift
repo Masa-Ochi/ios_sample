b07824287acc28a4cfdf60b049bef419258ec7fd
Masa <masa.ochi.74@gmail.com>
2019-10-05 10:38:46 +0900

    add comment

--- Sample/Sample/AppDelegate.swift
+++ Sample/Sample/AppDelegate.swift
@@ -19,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         return true
         // コメント追加
+        // サンプル２
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
